518232bbd0f55fe26e5e9ac4608f41b58ef4e599
AikanaroLossehelin <113172830+AikanaroLossehelin@users.noreply.github.com>
2023-12-23 15:44:36 +0100

    Automatisation des tests et couverture plus grande des fonctions du package

--- __init__.py
+++ __init__.py
@@ -3,81 +3,115 @@
 Created on Mon Oct 16 17:56:59 2023
 
 @author: aikan
+
+coverage run -m __init__
+coverage report
+coverage html
 """
-
-
 import unittest as ut
 import numpy as np
 import sys
-
-def generate_test_data():
-    test_cases = []
-
-    # Cas typiques
-    test_cases.append(([1, 2, 3], "Simple list"))  # Liste simple
-    test_cases.append(([1.1, 2.2, 3.3], "List of floats"))  # Liste de flottants
-    test_cases.append(([(1,2), (3,4)], "List of tuples"))  # Liste de tuples
-    test_cases.append(([], "Empty list"))  # Liste vide
-
-    # Cas avec différents types de données
-    test_cases.append((['a', 'b', 'c'], "List of strings"))  # Liste de chaînes
-    test_cases.append(([True, False, True], "List of booleans"))  # Liste de booléens
-
-    # Cas limites
-    test_cases.append((list(range(1000000)), "Large list"))  # Très grande liste
-    test_cases.append(([[1, 2], [3, 4, 5]], "Jagged array"))  # Array irrégulier
-
-    # Cas de données complexes ou spéciaux
-    test_cases.append((np.zeros((2, 3)), "2D array of zeros"))  # Array 2D de zéros
-    test_cases.append((1 + 2j, "Complex number"))  # Nombre complexe
-
-    # Cas extrêmes
-    test_cases.append((sys.maxsize, "Maximum integer size"))  # Taille max d'un entier
-    test_cases.append((np.inf, "Infinity"))  # Infini
-    test_cases.append((np.nan, "NaN"))  # Not a Number
-
-    return test_cases
-
-def test_numpy_array():
-    test_cases = generate_test_data()
-
-    for data, description in test_cases:
-        try:
-            array = np.array(data)
-            print(f"Test passed for: {description}")
-            print(f"Array: {array}\n")
-        except Exception as e:
-            print(f"Test failed for: {description}")
-            print(f"Error: {e}\n")
-
-# Exécuter les tests
-test_numpy_array()
-
+from hypothesis import given, strategies as st
 
 class TestNumpyFunctions(ut.TestCase):
 
-    def test_numpy_array(self):
-        # Cas typiques
-        self.assertTrue(np.array_equal(np.array([1, 2, 3]), np.array([1, 2, 3])))
-        # Cas limites et d'erreur...
+    @given(st.lists(st.integers()))
+    def test_numpy_array_with_integers(self, lst):
+        array = np.array(lst)
+        self.assertTrue(np.array_equal(array, np.array(lst)))
 
-    def test_numpy_zeros(self):
-        self.assertTrue(np.array_equal(np.zeros(3), np.array([0, 0, 0])))
-        # Autres cas...
+    @given(st.lists(st.floats(allow_nan=False, allow_infinity=False)))
+    def test_numpy_array_with_floats(self, lst):
+        array = np.array(lst)
+        self.assertTrue(np.array_equal(array, np.array(lst)))
 
-    def test_numpy_ones(self):
-        self.assertTrue(np.array_equal(np.ones(3), np.array([1, 1, 1])))
-        # Autres cas...
+    @given(st.lists(st.tuples(st.integers(), st.integers())))
+    def test_numpy_array_with_tuples(self, lst):
+        array = np.array(lst)
+        self.assertTrue(np.array_equal(array, np.array(lst)))
 
-    def test_numpy_arange(self):
-        self.assertTrue(np.array_equal(np.arange(3), np.array([0, 1, 2])))
-        # Autres cas...
+    @given(st.lists(st.booleans()))
+    def test_numpy_array_with_booleans(self, lst):
+        array = np.array(lst)
+        self.assertTrue(np.array_equal(array, np.array(lst)))
+        
+        # Tests pour la création d'arrays
+    @given(st.lists(st.integers()))
+    def test_numpy_array(self, lst):
+        array = np.array(lst)
+        self.assertTrue(np.array_equal(array, np.array(lst)))
 
-    def test_numpy_linspace(self):
-        self.assertTrue(np.array_equal(np.linspace(1, 3, 3), np.array([1, 2, 3])))
-        # Autres cas...
+    @given(st.integers(min_value=1, max_value=10))
+    def test_numpy_zeros(self, n):
+        self.assertTrue(np.array_equal(np.zeros(n), np.zeros(n)))
 
-    # Tests similaires pour les autres fonctions...
+    @given(st.integers(min_value=1, max_value=10))
+    def test_numpy_ones(self, n):
+        self.assertTrue(np.array_equal(np.ones(n), np.ones(n)))
+
+    @given(st.integers(min_value=0, max_value=10), st.integers(min_value=11, max_value=20))
+    def test_numpy_arange(self, start, end):
+        self.assertTrue(np.array_equal(np.arange(start, end), np.arange(start, end)))
+
+    @given(st.floats(min_value=0, max_value=10), st.floats(min_value=11, max_value=20), st.integers(min_value=2, max_value=10))
+    def test_numpy_linspace(self, start, end, num):
+        self.assertTrue(np.array_equal(np.linspace(start, end, num), np.linspace(start, end, num)))
+
+    # Tests pour les opérations mathématiques
+    @given(st.lists(st.floats(min_value=0, max_value=1e307, allow_nan=False, allow_infinity=False)), st.lists(st.floats(min_value=-1e307, max_value=1e307, allow_nan=False, allow_infinity=False)))
+    def test_numpy_operations(self, a, b):
+       if len(a) == len(b) and all(x != 0 for x in b):
+           a, b = np.array(a), np.array(b)
+           self.assertTrue(np.all(np.add(a, b) == np.add(a, b)))
+           self.assertTrue(np.all(np.subtract(a, b) == np.subtract(a, b)))
+           self.assertTrue(np.all(np.multiply(a, b) == np.multiply(a, b)))
+           self.assertTrue(np.all(np.divide(a, b) == np.divide(a, b)))
+           self.assertTrue(np.all(np.sqrt(a) == np.sqrt(a)))
+           self.assertTrue(np.all(np.exp(a) == np.exp(a)))
+           self.assertTrue(np.all(np.sin(a) == np.sin(a)))
+           self.assertTrue(np.all(np.cos(a) == np.cos(a)))
+           self.assertTrue(np.all(np.tan(a) == np.tan(a)))
+           
+    # Tests pour la manipulation d'arrays
+    @given(st.lists(st.integers()), st.integers(min_value=1, max_value=5))
+    def test_numpy_reshape(self, lst, n):
+        if len(lst) % n == 0 and len(lst) != 0:
+            a = np.array(lst).reshape(n, -1)
+            self.assertEqual(a.shape[0], n)
+
+    @given(st.lists(st.integers()), st.lists(st.integers()))
+    def test_numpy_concatenate(self, a, b):
+        a, b = np.array(a), np.array(b)
+        self.assertTrue(np.array_equal(np.concatenate([a, b]), np.concatenate([a, b])))
+
+    @given(st.lists(st.integers()), st.integers(min_value=2, max_value=5))
+    def test_numpy_split(self, lst, n):
+        if len(lst) >= n and len(lst) % n == 0:
+            splits = np.split(np.array(lst), n)
+            self.assertEqual(len(splits), n)
+
+    @given(st.lists(st.integers()))
+    def test_numpy_transpose(self, lst):
+        a = np.array(lst)
+        self.assertTrue(np.array_equal(a.transpose(), a.transpose()))
+
+    # Tests pour les statistiques et les aggrégations
+    @given(st.lists(st.floats(allow_infinity=False, allow_nan=False)))
+    def test_numpy_statistics(self, lst):
+        if len(lst) > 0:
+            a = np.array(lst)
+            self.assertAlmostEqual(np.mean(a), np.mean(a))
+            self.assertAlmostEqual(np.median(a), np.median(a))
+            self.assertAlmostEqual(np.std(a), np.std(a))
+            self.assertAlmostEqual(np.sum(a), np.sum(a))
+
+    # Tests pour l'algèbre linéaire
+    @given(st.lists(st.lists(st.floats(min_value=-100, max_value=100, allow_nan=False, allow_infinity=False), min_size=2, max_size=2), min_size=2, max_size=2))
+    def test_numpy_linear_algebra(self, lst):
+       a = np.array(lst)
+       if np.linalg.det(a) != 0 and not np.isclose(np.linalg.det(a), 0):
+           inv_a = np.linalg.inv(a)
+           self.assertTrue(np.allclose(np.dot(a, inv_a), np.eye(a.shape[0]), atol=1e-5))
 
 if __name__ == '__main__':
     ut.main()
